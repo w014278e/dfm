fa68d38b67dbfe59eb7fa4e729d7d3969197c98b
WILLIAMS Grant <w014278e@student.staffs.ac.uk>
2017-03-14 16:08:53 +0000

    service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,10 +1,18 @@
+var CACHE_NAME = 'gih-cache';
+var CACHED_URLS = [
+  'offline.html',
+  'mystyles.css’,
+  'dino.png'
+];
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('gih-cache').then(function(cache) {
-      return cache.add('offline.html');
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
     })
   );
 });
+
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
