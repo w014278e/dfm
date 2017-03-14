390448bb119b5ef89cfb7ef482a24b0d830d1c07
WILLIAMS Grant <w014278e@student.staffs.ac.uk>
2017-03-14 16:05:01 +0000

    service worker

--- serviceworker.js
+++ serviceworker.js
@@ -5,3 +5,11 @@ self.addEventListener('install', function(event) {
     })
   );
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match('offline.html');
+    })
+  );
+});
