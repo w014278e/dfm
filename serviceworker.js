bf13dd92236d569f3a16c52168a1979044180000
WILLIAMS Grant <w014278e@student.staffs.ac.uk>
2017-03-14 16:02:43 +0000

    service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,18 +1,7 @@
-var responseContent = '<html>' +
-  '<body>' +
-  '<style>' +
-  'body {text-align: center; background-color: #333; color: #eee;}' +
-  '</style>' +
-  '<h1>Paddys page</h1>' +
-  '<p>There seems to be a problem with your connection.</p>' +
-  '<p>Come visit us at Fred Street for free WiFi.</p>' +
-  '</body>' +
-  '</html>';
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return new Response(responseContent, {headers: {'Content-Type': 'text/html'}});
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('gih-cache').then(function(cache) {
+      return cache.add('offline.html');
     })
   );
 });
