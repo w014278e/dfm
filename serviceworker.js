2fdf6e71072c7c127b7e9f84af5d37db74246ca6
WILLIAMS Grant <w014278e@student.staffs.ac.uk>
2017-03-14 15:40:03 +0000

    service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,8 +1,7 @@
 self.addEventListener('fetch', function(event) {
-  if (event.request.url.indexOf('material.teal-red.min.css') !== -1) {
+  if (event.request.url.indexOf('paddy.jpg') !== -1) {
     event.respondWith(
-      new Response('body { background: green; }',
-      { headers: { 'Content-Type': 'text/css' } }
-    ));
+      fetch('gerbil.jpg')
+    );
   }
-});
+})
