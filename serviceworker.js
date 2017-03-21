392936d398c1b6e11cc39de6bfc1309a97a80c61
WILLIAMS Grant <w014278e@student.staffs.ac.uk>
2017-03-21 15:33:54 +0000

    service worker states

--- serviceworker.js
+++ serviceworker.js
@@ -9,7 +9,7 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
   if (event.request.url.indexOf('material.teal-red.min.css') !== -1) {
     console.log('Fetch request for:', event.request.url);
-    event.respondWith(new Response('header {background: green url("")!important}', {
+    event.respondWith(new Response('header {background: red url("")!important}', {
       headers: { 'Content-Type': 'text/css' }
     }));
   }
