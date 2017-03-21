cc9eaccccd65d0aab5a27df4166ff6ab25725633
WILLIAMS Grant <w014278e@student.staffs.ac.uk>
2017-03-21 15:20:09 +0000

    service worker states

--- serviceworker.js
+++ serviceworker.js
@@ -1,28 +1,16 @@
-var CACHE_NAME = 'gih-cache';
-var CACHED_URLS = [
-  'first.html',
-  'styles.css',
-  'paddy.jpeg'
-];
-
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(CACHED_URLS);
-    })
-  );
+  console.log('install');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('activate');
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get('accept').includes('text/html')) {
-          return caches.match('first.html');
-        }
-      });
-    })
-  );
+  if (event.request.url.indexOf('material.teal-red.min.css') !== -1) {
+    console.log('Fetch request for:', event.request.url);
+    event.respondWith(new Response('header {background: green url("")!important}', {
+      headers: { 'Content-Type': 'text/css' }
+    }));
+  }
 });
